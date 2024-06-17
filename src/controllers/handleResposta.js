ef92463689476b31d4a6b1526b42e33110ecec51
andre felipe wonsik alves <aalves.2020@alunos.utfpr.edu.br>
2024-06-17 16:38:37 -0300

    update all

--- src/controllers/handleResposta.js
+++ src/controllers/handleResposta.js
@@ -1,35 +1,34 @@
-let options = [2, 3, 1]
-let resposta = 2
+let options = [2, 3, 1];
+let resposta = 2;
 
 const arrPerguntas = ["1+1", "2^3", "2/2"];
 const arrRespostas = [2, 8, 1];
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
-
-  
 });
 
 // recebe o index vindo do botão (0, 1 ou 2) que seria correspondente ao index da opção no array
-function checarResposta(button){
-  console.log(options[button])
-  if (options[button] == resposta){
-    console.log('correto');
-    novaQuestao();
-  }
+function checarResposta(button) {
+  console.log(options[button]);
+  if (options[button] == resposta) return novaQuestao();
+  errou();
 }
 
+function novaQuestao() {
+  //exibir nova pergunta, atualizar as opcoes e diminuir a vida do mosntro (?)
+}
 
-function novaQuestao(){
-  //exibir nova pergunta, atualizar as opcoes e diminuir a vida do mosntro (?) 
+function errou(){
+  
 }
 
 //coloca as opções nos botões
 //futuramente colocar dinamicamente a pergunta para não ser sempre a mesma
-function init(){
+function init() {
   for (let i = 0; i < 3; i++) {
-    let alterando = "botao"+i.toString()
-    console.log(alterando)
-    document.getElementById(alterando).innerHTML = options[i]
+    let alterando = "botao" + i.toString();
+    console.log(alterando);
+    document.getElementById(alterando).innerHTML = options[i];
   }
 }
