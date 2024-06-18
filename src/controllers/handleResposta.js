fe0b80ea3643083dfa35d3b35f3dda2912a2f835
andre felipe wonsik alves <aalves.2020@alunos.utfpr.edu.br>
2024-06-18 13:36:53 -0300

    create hitmonstro e hitplayer

--- src/controllers/handleResposta.js
+++ src/controllers/handleResposta.js
@@ -1,11 +1,13 @@
 let options = [2, 3, 1];
 let resposta = 2;
 
-let hp_atual = 50
-let hp_total = 50
-
 const arrPerguntas = ["1+1", "2^3", "2/2"];
 const arrRespostas = [2, 8, 1];
+
+const hpMonstro = 5;
+const hpPlayer = 3;
+let hitsPlayer = 1;
+let hitsMonstro = 1;
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,27 +15,30 @@ document.getElementById("form").addEventListener("submit", (e) => {
 
 // recebe o index vindo do botão (0, 1 ou 2) que seria correspondente ao index da opção no array
 function checarResposta(button) {
-  console.log(options[button]);
   if (options[button] == resposta) return novaQuestao();
-  errou();
+  hitPlayer();
 }
 
 // -------------------------- SIGMA
 function novaQuestao() {
-  //exibir nova pergunta, atualizar as opcoes e diminuir a vida do mosntro (?)
-  hp_atual -= 5;
-  document.getElementById("hp_monstro").innerHTML = "VIDA: "+ hp_atual+"/"+hp_total;
+  hitMonstro()
+  //da um hit no monstro e dps chama
 }
 
 // -------------------------- ANDREZAO
-function errou(){
-  //diminuiu uma vida
-  // novaQuestao();
+function hitPlayer() {
+  if(hitsPlayer >= hpPlayer) return false;
+
+  document.querySelector("#hp_player").innerHTML = "❤".repeat(hpPlayer - hitsPlayer);
+  hitsPlayer++;
 }
 
 // -------------------------- ANDREZAO
-function vidaMonstro(){
+function hitMonstro() {
+  if(hitsMonstro >= hpMonstro) return false; //TROCAR O MONSTRO
 
+  document.querySelector("#hp_monstro").innerHTML = "❤".repeat(hpMonstro - hitsMonstro);
+  hitsMonstro++;
 }
 
 //coloca as opções nos botões
@@ -42,11 +47,12 @@ function vidaMonstro(){
 function init() {
   document.getElementById("conta").innerHTML = arrPerguntas[2];
 
-  document.getElementById("hp_monstro").innerHTML = "VIDA: "+ hp_atual+"/"+hp_total;
+  // Vidas
+  document.querySelector("#hp_monstro").innerHTML = "❤".repeat(hpMonstro);
+  document.querySelector("#hp_player").innerHTML = "❤".repeat(hpPlayer);
 
   for (let i = 0; i < 3; i++) {
     let alterando = "botao" + i.toString();
-    console.log(alterando);
     document.getElementById(alterando).innerHTML = options[i];
   }
 }
